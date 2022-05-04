13854da0028ac58befb314686d40e961071a7dec
GeniusDP <zaranikbz@gmail.com>
2022-05-04 20:02:08 +0300

    deleted default realization of fileAll method in ModelLayer class

--- src/main/java/kpi/third/term/java/lab/customers/models/repositories/ModelLayer.java
+++ src/main/java/kpi/third/term/java/lab/customers/models/repositories/ModelLayer.java
@@ -3,23 +3,10 @@ package kpi.third.term.java.lab.customers.models.repositories;
 import kpi.third.term.java.lab.customers.models.entities.Customer;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ModelLayer {
 
-    default List<Customer> findAll(File file){
-        List<Customer> customers = new ArrayList<>();
-
-        for(int i = 0; i < 20; i++){
-            customers.add( CustomerFactory.getCustomerInstance() );
-        }
-        return customers;
-    }
-
+    List<Customer> findAll(File file);
 
 }
-
-
-
-
