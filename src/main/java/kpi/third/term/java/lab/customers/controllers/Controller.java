5515750a5a0e22f693607d8ad0707b84c5e20fba
GeniusDP <zaranikbz@gmail.com>
2022-05-19 13:00:54 +0300

    Service now owns repository

--- src/main/java/kpi/third/term/java/lab/customers/controllers/Controller.java
+++ src/main/java/kpi/third/term/java/lab/customers/controllers/Controller.java
@@ -17,7 +17,6 @@ import java.util.Properties;
 
 public class Controller {
 
-    private final Repository repository;
     private final ViewLayer view;
     private final CustomerService service;
     private static final Logger logger = LogManager.getLogger(Controller.class);
@@ -32,8 +31,7 @@ public class Controller {
             System.exit(1337);
         }
         view = new ViewLayer();
-        repository = new JSONRepository( new File( properties.getProperty("main-file-path") ) );
-        service = new CustomerService();
+        service = new CustomerService( new JSONRepository( new File( properties.getProperty("main-file-path") ) ) );
     }
 
 
@@ -42,16 +40,16 @@ public class Controller {
         view.printMessage( ViewLayer.HELLO_MESSAGE );
 
         view.printMessage( ViewLayer.CUSTOMERS_LIST_FETCHED_MESSAGE );
-        view.printCustomerList( repository.findAll( ) );
+        view.printCustomerList( service.findAll( ) );
 
         do{
             OperationType operationType = view.getOperationType();
             List<Customer> currentCustomersList = switch( operationType ){
-                case ALPHABETIC_ORDER -> service.getCustomersInAlphabeticOrder(repository.findAll( ));
+                case ALPHABETIC_ORDER -> service.getCustomersInAlphabeticOrder();
                 case CARD_NUMBER_RANGE -> {
                     long leftBound = view.getLeftBoundOfRange();
                     long rightBound = view.getRightBoundOfRange();
-                    yield service.getCustomersByCardNumberInRange(repository.findAll( ), leftBound, rightBound);
+                    yield service.getCustomersByCardNumberInRange(leftBound, rightBound);
                 }
             };
             view.printCustomerList( currentCustomersList );
@@ -59,11 +57,10 @@ public class Controller {
             boolean savingDesired = view.saveDialog();
 
             if( savingDesired ){
-                //засунуть в сервис
                 File fileToSave = view.saveFileGetting();
                 if( fileToSave != null ){
                     logger.info("Result of current computation saved.");
-                    repository.saveAll(fileToSave, currentCustomersList);
+                    service.saveAll(fileToSave, currentCustomersList);
                     view.printMessage( ViewLayer.EVERYTHING_SAVED  );
                 }
             }
--- src/main/java/kpi/third/term/java/lab/customers/models/service/CustomerService.java
+++ src/main/java/kpi/third/term/java/lab/customers/models/service/CustomerService.java
@@ -1,23 +1,45 @@
 package kpi.third.term.java.lab.customers.models.service;
 
 import kpi.third.term.java.lab.customers.models.entities.Customer;
+import kpi.third.term.java.lab.customers.models.repositories.Repository;
 
+import java.io.File;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CustomerService {
 
-    public List<Customer> getCustomersInAlphabeticOrder(List<Customer> customers) {
+    private Repository repository;
+
+    public CustomerService(Repository repository) {
+        this.repository = repository;
+    }
+
+    public List<Customer> getCustomersInAlphabeticOrder() {
+        List<Customer> customers = repository.findAll();
         return customers.stream().sorted( Customer::compareTo ).collect( Collectors.toList() );
     }
 
-
-    public List<Customer> getCustomersByCardNumberInRange(List<Customer> customers, long leftBound, long rightBound) {
+    public List<Customer> getCustomersByCardNumberInRange(long leftBound, long rightBound) {
+        List<Customer> customers = repository.findAll();
         Predicate<Customer> filteringPredicate = customer -> {
             return customer.getBankCardNumber() >= leftBound && customer.getBankCardNumber() <= rightBound;
         };
         return customers.stream().filter( filteringPredicate ).collect( Collectors.toList() );
     }
+
+    public List<Customer> findAll(){
+        return repository.findAll();
+    }
+
+    public void saveAll(File fileToSave, List<Customer> currentCustomersList) {
+        repository.saveAll(fileToSave, currentCustomersList);
+    }
+
+    public Repository getRepository() {
+        return repository;
+    }
+
 
 }
